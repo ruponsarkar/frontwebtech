57d2b4d67f46fe3f4a9e9206a8f249a58da0276b
Rupon Sarkar <ruponsarkar108@gmail.com>
2026-05-20 15:00:05 +0530

    Enable standalone output

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -1,10 +1,7 @@
-import path from "node:path";
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  reactStrictMode: true,
   output: "standalone",
-  outputFileTracingRoot: path.join(__dirname, ".."),
 };
 
 export default nextConfig;
